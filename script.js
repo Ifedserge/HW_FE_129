6c9a29fa2616cd99e1b4fd789b28d633e716ebbc
Serge Fedorov <Lir2ooo5@yandex.ru>
2023-02-10 00:45:48 +0300

    add hw

--- script.js
+++ script.js
@@ -1,52 +1,90 @@
-// задание 2
-let a1 = 5 % 3;
-let a2 = 3 % 5;
-let a3 = 5 + '3';
-let a4 = '5' - 3;
-let a5 = 75 + 'кг';
-let a6 = 785 * 653;
-let a7 = 100 / 25;
-let a8 = 0 * 0;
-let a9 = 0 / 2;
-let a10 = 89 / 0;
-let a11 = 98 + 2;
-let a12 = 5 - 98;
-let a13 = (8 + 56 * 4) / 5;
-let a14 = (9 - 12) * 7 / (5 + 2);
-let a15 = 1 || 0;
-let a16 = false || true;
-let a17 = true > 0;
-console.log(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a15, a16, a17);
+console.log('1. Сделайте функцию, которая отнимает от первого числа второе и делит на третье. Числа передаются параметром.');
+function abc(a, b, c){
+    return (a - b) / c;
+}
+console.log(abc(10, 2, 2));
 
-// задание 3
-let height = 23;
-let width = 10;
-let SPryam = height * width;
-console.log(SPryam + 'см');
+console.log('');
+console.log("2. Сделайте функцию, которая возвращает куб числа и его квадрат. Число передается параметром.");
 
-// задание 4
-let Hcil = 10;
-let VCilindra = 3.14 * Math.pow(a7, 2) * Hcil;
-console.log(VCilindra)
+function retCQ(a){
+    return ('куб = ' + Math.pow(a, 3) + ' квадрат числа = ' + Math.pow(a, 2));
+}
+console.log(retCQ(3));
 
-//  задание 5
- let Rkrug = 5; 
- let Skrug = 3.14 * Math.pow(Rkrug, 2);
- console.log(Skrug)
+console.log("");
+console.log('3. Напишите функции min и max, которые возвращают меньшее и большее из чисел a и b.');
+function min(a, b){
+    if(a < b) return a;
+    else return b;
+}
+console.log(min(9, 6));
 
-//  задание 6
+function max(a, b){
+    if(a > b) return a;
+    else return b;
+}
+console.log(max(1, 0));
 
-// let a = 5;
-// let b = 7;
-// let Htrap = 10;
-// let STrap = ((a + b) / 2 ) * Htrap;
-// console.log(STrap + 'см')
+console.log("");
+console.log('4. Напишите две функции: первая ф-ция должна возвращать массив числовыми значениями, диапазон которых должен вводиться пользователем с клавиатуры; вторая – выводить полученный массив.');
 
-// задание 7
-let S = 2000000;
-let years = 5;
-let p = 10;
-let Pereplata = (S / 100 * p) * years;
-console.log(Pereplata)
+let arr4 = [];
+function getArrNumb(a, b){
 
- 
+var a = +prompt("Введите певрое число");
+var b = +prompt("Введите второе число");
+     for(let i = a; i <= b; i++){
+        arr4.push(i);
+     }
+    return arr4;
+}
+console.log(getArrNumb());
+
+function array(a){
+    for(let i = 0; i < a.length; i++){
+        console.log(a[i]);
+    }
+}
+
+console.log(array(arr4));
+
+
+console.log("");
+console.log('5. Сделайте функцию isEven() (even - это четный), которая параметром принимает целое число и проверяет: четное оно или нет. Если четное - пусть функция возвращает true, если нечетное — false.');
+function isEven(a){
+    return (a % 2 == 0) ? true : false;
+    
+}
+console.log(isEven(4));
+
+console.log("");
+console.log('6. Напишите ф-цию, в которую передается массив с целыми числами. Верните новый массив, где останутся лежать только четные из этих чисел. Для этого используйте вспомогательную функцию isEven из предыдущей задачи.');
+let arr6 = [];
+function exp6(r){
+    for(let i = 0; i <= r.length; i++){
+       if(isEven(r[i]) !== true) arr6.push(i);
+    }
+    return arr6
+};
+console.log(exp6(arr4));
+
+console.log("");
+console.log('9. Напишите ф-цию, которая возвращает массив заполненный числами Фибоначи от 0 до 1000.');
+let arr9 = [];
+function numbFib(){
+    
+    for(let i = 0; i < 16; i++){
+        arr9.push(i)
+        if(arr9[i] == 0 || arr9[i] == 1){
+            arr9[i] = 1;
+        }else if(arr9[i] <= 1000){
+            arr9[i] = arr9[i - 1] + arr9[i-2];
+        }else{
+            break
+        }
+    }
+    return arr9;
+}
+
+    console.log(numbFib());
