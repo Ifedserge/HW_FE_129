f2753d1264c377773a24cc5053be3ace099b72e6
Serge Fedorov <Lir2ooo5@yandex.ru>
2023-02-17 00:28:27 +0300

    add HW 7

--- script.js
+++ script.js
@@ -1,52 +1,75 @@
-// задание 2
-let a1 = 5 % 3;
-let a2 = 3 % 5;
-let a3 = 5 + '3';
-let a4 = '5' - 3;
-let a5 = 75 + 'кг';
-let a6 = 785 * 653;
-let a7 = 100 / 25;
-let a8 = 0 * 0;
-let a9 = 0 / 2;
-let a10 = 89 / 0;
-let a11 = 98 + 2;
-let a12 = 5 - 98;
-let a13 = (8 + 56 * 4) / 5;
-let a14 = (9 - 12) * 7 / (5 + 2);
-let a15 = 1 || 0;
-let a16 = false || true;
-let a17 = true > 0;
-console.log(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a15, a16, a17);
+// console.log('1. Калькулятор. Возможные методы: вкл/выкл калькулятора, получение выражения для расчета или, а и б числа с операцией, вычисление выражения, показ результата расчета.');
 
-// задание 3
-let height = 23;
-let width = 10;
-let SPryam = height * width;
-console.log(SPryam + 'см');
 
-// задание 4
-let Hcil = 10;
-let VCilindra = 3.14 * Math.pow(a7, 2) * Hcil;
-console.log(VCilindra)
+// let Calc = function(){
+//     let power = prompt('Включить калькулятор? on/off')
+//     if(power == 'on' || power === true){
+//         this.get = function(){
+//             this.a = +prompt('Введите число "А"');
+//             this.b = +prompt('Введите число "b"');
+//             this.oper = prompt('введите операцию (+/-/*///)')
+    
+//             this.operation();
+//         }
+//         this.operation = function(){
+//             switch(this.oper){
+//                 case '+':
+//                     this.result = this.a + this.b;
+//                 break;
+//                 case '-':
+//                     this.result = this.a - this.b;
+//                 break;
+//                 case '*':
+//                     this.result = this.a * this.b;
+//                 break;
+//                 case '/':
+//                     this.result = this.a / this.b;
+//                 break;
+//                 default: this.result = 0
+//             }
+//             this.show();
+//         }
+//         this.show = function(){
+//             alert(this.a + ' ' + this.oper + ' ' + this.b + ' = ' + this.result )
+//         }
+//     }
+// }
 
-//  задание 5
- let Rkrug = 5; 
- let Skrug = 3.14 * Math.pow(Rkrug, 2);
- console.log(Skrug)
+// let calc = new Calc();  
+// // calc.get()
 
-//  задание 6
+// console.log('2. Лампочка. Возможные методы: ввод информации о лампочке (мощность, стоимость электроэнергии), вкл./выкл. лампочки, получение расхода за горение лампочки, счетчик горения лампочки.');
 
-// let a = 5;
-// let b = 7;
-// let Htrap = 10;
-// let STrap = ((a + b) / 2 ) * Htrap;
-// console.log(STrap + 'см')
+// let Lamp = function(){
+//     this.get = function(){
+//         this.powerWt = +prompt('Введите мощность лампочки в Вт');
+//         this.time = prompt("Сколько часов работает лампочка")
+//         this.rate = 0.1;
+//         this.result = this.powerWt/1000 * this.time * this.rate;
+//         alert(this.powerWt + this.time)   
+//     }
+        
+// }
+// let lamp = new Lamp();
+// lamp.get()
 
-// задание 7
-let S = 2000000;
-let years = 5;
-let p = 10;
-let Pereplata = (S / 100 * p) * years;
-console.log(Pereplata)
 
- 
+console.log('4. Автомобиль. Возможные методы: ввод информации об авто (марка, номер), вкл./выкл. двигателя, вкл./выкл. передачи (вперед, назад, нейтральная), движение вперед и назад (ввод информации о скорости движения), расчет пройденных километров.');
+
+let Car = function (){
+    this.marka = prompt('Введите марку автомобиля');
+    this.model = prompt('Введите модель');
+    this.rashod = +prompt('Укажите средний расход на 100км');
+    this.get = function (){
+        this.way = +prompt('Укажите растояние');
+        this.price = +prompt('Сколько стоит топливо?')
+        this.resultLitr = this.way / 100 * this.rashod
+
+        this.resultMoney = this.resultLitr * this.price
+        alert("Израсходавано топлива " + this.resultLitr);
+        alert("Стоимость " + this.resultMoney);
+    }
+}
+
+let car = new Car()
+car.get();
