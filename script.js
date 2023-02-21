0b7c3073cab6fdebc278e2abdcca6221da7ca081
Serge Fedorov <Lir2ooo5@yandex.ru>
2023-02-21 03:02:44 +0300

    add hw8

--- script.js
+++ script.js
@@ -1,52 +1,211 @@
-// задание 2
-let a1 = 5 % 3;
-let a2 = 3 % 5;
-let a3 = 5 + '3';
-let a4 = '5' - 3;
-let a5 = 75 + 'кг';
-let a6 = 785 * 653;
-let a7 = 100 / 25;
-let a8 = 0 * 0;
-let a9 = 0 / 2;
-let a10 = 89 / 0;
-let a11 = 98 + 2;
-let a12 = 5 - 98;
-let a13 = (8 + 56 * 4) / 5;
-let a14 = (9 - 12) * 7 / (5 + 2);
-let a15 = 1 || 0;
-let a16 = false || true;
-let a17 = true > 0;
-console.log(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a15, a16, a17);
+let html = document.querySelector('html')
+html.setAttribute('lang', 'en')
 
-// задание 3
-let height = 23;
-let width = 10;
-let SPryam = height * width;
-console.log(SPryam + 'см');
+let title  = document.createElement('title');
+title.innerHTML = 'HW 8';
+document.head.appendChild(title);
 
-// задание 4
-let Hcil = 10;
-let VCilindra = 3.14 * Math.pow(a7, 2) * Hcil;
-console.log(VCilindra)
+let metaUtf8 = document.createElement('meta');
+metaUtf8.setAttribute('charset', 'UTF-8')
+document.head.appendChild(metaUtf8);
 
-//  задание 5
- let Rkrug = 5; 
- let Skrug = 3.14 * Math.pow(Rkrug, 2);
- console.log(Skrug)
+let metaEquiv = document.createElement('meta');
+metaEquiv.setAttribute('http-equiv', 'X-UA-Compatible')
+metaEquiv.setAttribute('content', 'IE=edge')
+document.head.appendChild(metaEquiv);
 
-//  задание 6
+let metaViewport = document.createElement('meta');
+metaViewport.setAttribute('name', 'viewport');
+metaViewport.setAttribute('content', 'width=device-width, initial-scale=1.0');
+document.head.appendChild(metaViewport);
 
-// let a = 5;
-// let b = 7;
-// let Htrap = 10;
-// let STrap = ((a + b) / 2 ) * Htrap;
-// console.log(STrap + 'см')
+let style = document.createElement('style');
+document.head.appendChild(style);
 
-// задание 7
-let S = 2000000;
-let years = 5;
-let p = 10;
-let Pereplata = (S / 100 * p) * years;
-console.log(Pereplata)
+let script = document.querySelector('script')
+style.after(script);
 
- 
+let divSite = document.createElement('div');
+divSite.setAttribute('class', 'site');
+document.body.appendChild(divSite)
+
+
+let divContainer = document.createElement('div');
+divContainer.classList.add('container');
+
+
+
+// header block
+
+let header = document.createElement('header')
+header.classList.add('header');
+divSite.appendChild(header);
+header.appendChild(divContainer);
+
+let headerWraper = document.createElement('div');
+headerWraper.classList.add('header__container-wraper')
+divContainer.appendChild(headerWraper);
+
+let h1 = document.createElement('h1')
+h1.innerHTML = 'Choose Your Option'
+headerWraper.appendChild(h1);
+
+let headerText = document.createElement('span');
+headerText.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing';
+headerWraper.appendChild(headerText)
+
+// main block
+
+let main = document.createElement('main');
+divSite.appendChild(main);
+
+let section = document.createElement('section');
+section.classList.add('content');
+main.appendChild(section);
+
+let divContainerSection = document.createElement('div');
+divContainerSection.classList.add('container');
+section.appendChild(divContainerSection)
+
+let contentWraper = document.createElement('div');
+contentWraper.classList.add('content__container-wraper');
+
+divContainerSection.appendChild(contentWraper);
+
+
+let block1 = document.createElement('div')
+block1.classList.add('block1');
+contentWraper.appendChild(block1);
+
+let block1Span = document.createElement('span');
+block1Span.innerHTML = 'FREELANCER';
+block1.appendChild(block1Span);
+
+let block1Title = document.createElement('h2');
+block1Title.innerHTML = 'Initially designed to ';
+block1.appendChild(block1Title);
+
+let block1P = document.createElement('p')
+block1P.innerHTML = 'But I must explain to you how all this mistaken idea of denouncing ';
+block1.appendChild(block1P);
+
+let block1Button = document.createElement('button')
+block1Button.innerHTML = "START HERE"
+block1.appendChild(block1Button);
+
+let block2 = document.createElement('div')
+block2 = block1.cloneNode(true)
+contentWraper.appendChild(block2);
+
+
+
+style.innerHTML = `
+    *{
+        padding: 0;
+        margin: 0;
+    }
+    .site{
+        width: 100%;
+        max-width: 1280px;
+        margin: 0 auto;
+    }
+    .container{
+        width: 800px;
+        margin: 0 auto;
+    }
+    .header{
+        padding-top: 122px;
+        margin-bottom: 55px
+    }
+    .header__container-wraper{
+        display: flex;
+        flex-direction: column;
+    }
+    .header__container-wraper h1{
+        font-family: 'Arvo';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 36px;
+        line-height: 48px;
+        display: flex;
+        align-items: center;
+        color: #212121;
+        margin: 0 auto;
+    }
+    .header__container-wraper span{
+        padding-top: 10px;
+        font-family: 'OpenSans';
+        font-size: 14px;
+        line-height: 26px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        color: #9FA3A7;
+        margin: 0 auto;
+    }
+    .content__container-wraper{
+        display: flex;
+        aligin-items: center;
+    }
+    .block1{
+        width: 400px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .block1 span{
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 12px;
+        line-height: 15px;
+        letter-spacing: 2.4px;
+        color: #9FA3A7;
+        padding-top: 81px;
+        margin-bottom: 19px;
+    }
+    .block1 h2{
+        font-family: 'Arvo';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 36px;
+        line-height: 46px;
+        color: #212121;
+        width: 210px;
+        text-align: center;
+        margin-bottom: 25px;
+    }
+    .block1 p{
+        display: block;
+        width: 210px;
+        font-family: 'OpenSans';
+        font-size: 12px;
+        line-height: 22px;
+        color: #9FA3A7;
+        text-align: center;
+        margin-bottom: 52px;
+    }
+    .block1 button{
+        width: 147px;
+        height: 46px;
+        border: 3px solid #FFC80A;
+        border-radius: 15px;
+        margin-bottom: 82px;
+    }
+    .block1:hover{
+        background: #8F75BE;
+    }
+    .block1:hover span{
+        color: #FFC80A;
+    }
+    .block1:hover h2{
+        color: #FFFFFF;
+    }
+    .block1:hover p{
+        color: #FFFFFF;
+    }
+    .block1:hover button{
+        background: #8F75BE;
+        color: #FFFFFF;
+    }
+`
